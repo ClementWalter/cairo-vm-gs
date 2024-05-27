fa2418681a7f5ebe9d9be8d0fca59a4fc8c5c8cf
KeneePatel <patelkenee2804@gmail.com>
2024-05-27 16:56:01 -0300

    added bitwise builtin functions

---
+++ src/builtin.ts
@@ -0,0 +1,19 @@
+// calculates bitwise 'AND' of given two inputs x and y
+function BITWISE_AND(x: bigint, y: bigint) {
+  return x & y;
+}
+
+// calculates bitwise 'OR' of given two inputs x and y
+function BITWISE_OR(x: bigint, y: bigint) {
+  return x | y;
+}
+
+// calculates bitwise 'XOR' of given two inputs x and y
+function BITWISE_XOR(x: bigint, y: bigint) {
+  return x ^ y;
+}
+
+// calculates bitwise 'AND', 'XOR' and 'OR' of given two inputs x and y
+function BITWISE_OPERATIONS(x: bigint, y: bigint) {
+  return [BITWISE_AND(x, y), BITWISE_XOR(x, y), BITWISE_OR(x, y)];
+}

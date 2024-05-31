57d7619ed5a59f9d952a2691b876ff72c7c45e64
Antoine FONDEUR <antoine.fondeur@etu.minesparis.psl.eu>
2024-05-31 14:33:59 +0200

    add ecdsa

--- src/builtins/ec_op/modular_ar.ts
+++ src/builtins/ec_op/modular_ar.ts
@@ -1,32 +1,32 @@
-function modAdd(a: bigint, b: bigint): bigint {
-  var sum: bigint = (a + b) % PRIME;
-  return sum < 0 ? sum + PRIME : sum;
+function modAdd(a: bigint, b: bigint, p: bigint=PRIME): bigint {
+  var sum: bigint = (a + b) % p;
+  return sum < 0 ? sum + p : sum;
 }
 
-function modSub(a: bigint, b: bigint): bigint {
-  var sum: bigint = (a - b) % PRIME;
-  return sum < 0 ? sum + PRIME : sum;
+function modSub(a: bigint, b: bigint, p: bigint=PRIME): bigint {
+  var sum: bigint = (a - b) % p;
+  return sum < 0 ? sum + p : sum;
 }
 
-function modMul(a: bigint, b: bigint): bigint {
-  var sum: bigint = (a * b) % PRIME;
-  return sum < 0 ? sum + PRIME : sum;
+function modMul(a: bigint, b: bigint, p: bigint=PRIME): bigint {
+  var sum: bigint = (a * b) % p;
+  return sum < 0 ? sum + p : sum;
 }
 
-function modInv(a: bigint): bigint {
-  return modExp(a, PRIME - BigInt(2));
+function modInv(a: bigint, p: bigint=PRIME): bigint {
+  return modExp(a, p - BigInt(2),p);
 }
 
-function modExp(base: bigint, exponent: bigint): bigint {
+function modExp(base: bigint, exponent: bigint, p: bigint=PRIME): bigint {
   var result = BigInt(1);
-  base = base % PRIME;
+  base = base % p;
 
   while (exponent > 0) {
     if (exponent % BigInt(2) === BigInt(1)) {
-      result = (result * base) % PRIME;
+      result = (result * base) % p;
     }
     exponent = exponent >> BigInt(1);
-    base = (base * base) % PRIME;
+    base = (base * base) % p;
   }
 
   return result;

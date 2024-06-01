4c209c7b56564a550cea4f12e7e516977c2fef28
KeneePatel <patelkenee2804@gmail.com>
2024-06-01 20:39:17 -0300

    rebased latest updates, refacto, added custom function in Code.ts file

--- src/builtins/range_check/range_check.ts
+++ src/builtins/range_check/range_check.ts
@@ -1,10 +1,10 @@
 const RC_BOUND: bigint = BigInt(2 ** 128);
 
 // performs a range check for the input num of type bigint.
-function RANGE_CHECK(num: bigint): bigint {
+function range_check(num: bigint): bigint {
   if (num < RC_BOUND) {
     return num;
   } else {
-    throw new RangeError("provided number was out of range");
+    throw new InvalidRangeError();
   }
 }

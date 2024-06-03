579da87f3a76a24b6bed94ba0fba82591ece4296
Clément Walter <clement0walter@gmail.com>
2024-06-03 17:05:01 +0200

    Update src/builtins/bitwise/bitwise.ts

--- src/builtins/bitwise/bitwise.ts
+++ src/builtins/bitwise/bitwise.ts
@@ -13,7 +13,3 @@ function bitwise_xor(x: bigint, y: bigint): bigint {
   return x ^ y;
 }
 
-// calculates bitwise 'AND', 'XOR' and 'OR' of given two inputs x and y
-function bitwise_operations(x: bigint, y: bigint): [bigint, bigint, bigint] {
-  return [bitwise_and(x, y), bitwise_xor(x, y), bitwise_or(x, y)];
-}

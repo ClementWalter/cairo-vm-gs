253bddc9078706794c20da83bf597f7d5ed3939e
Antoine FONDEUR <antoine.fondeur@etu.minesparis.psl.eu>
2024-10-05 10:38:14 -0400

    reverse array in trace flags

--- src/ui/Code.ts
+++ src/ui/Code.ts
@@ -185,7 +185,7 @@ function GET_FLAGS_AND_OFFSETS(encodedInstruction: string): number[][] {
   );
 
   let traceFlags: number[] = flags.map((f, i, arr) =>
-    parseInt(arr.slice(i).join()),
+    parseInt(arr.slice(i).reverse().join()),
   );
 
   return [[dstOffset, op0Offset, op1Offset, ...traceFlags]];
